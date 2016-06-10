4ab67d415446d11be43c3baa1d8d49d11d13b8c4
Alex Neuenkirk <t-alneue@microsoft.com>
2016-06-10 15:58:19 -0700

    Implemented site items in Query, added testing for new implementations.

--- Minerva/UnitTests.py
+++ Minerva/UnitTests.py
@@ -1,38 +1,53 @@
 import unittest
-import core
+from owl import stackexchangeparameters as separams, stackexchangequery as sequery
 
 class Test_Core(unittest.TestCase):
     # Class StackExchangeQuery.
     def test_classVariables(self):
         # QUESTION:  is there a more pythonic way than to use class variables?
-        assert(core.StackExchangeQuery.BASE_URL == 'https://api.stackexchange.com'), "Base URL mismatch."
-        assert(core.StackExchangeQuery.VERSION == '2.2'), "Version mismatch, testing against 2.2."
+        assert(sequery.Query.BASE_URL == 'https://api.stackexchange.com'), "Base URL mismatch."
+        assert(sequery.Query.VERSION == '2.2'), "Version mismatch, testing against 2.2."
 
     def test_instanceVariables(self):
-        instance = core.StackExchangeQuery()
+        instance = sequery.Query('stackoverflow')
         self.assertEqual(instance.startURL, 'https://api.stackexchange.com/2.2')
+
+    def test_setQueryType(self):
+        instance = sequery.Query('stackoverflow')
+        self.assertEqual(instance._queryType, 'questions')
+        self.assertRaises(ValueError, instance.setQueryType, 'unimplemented')
+
+    def test_getQueryType(self):
+        instance = sequery.Query('stackoverflow')
+        self.assertEqual(instance.getQueryType(), 'questions')
+
+    def test_setSite(self):
+        instance = sequery.Query('stackoverflow')
+        self.assertEqual(instance.getSite(), 'stackoverflow')
+        self.assertRaises(ValueError, sequery.Query, 'notAValidSite')
+
     
     # Class Parameters.
     def test_constructionWithoutParameters(self):
-        instance = core.Parameters()
+        instance = separams.Parameters()
         self.assertFalse(instance.getAsQueryString()), "instance.parameters should be empty, but it is not."
         self.assertEqual(instance.getSize(), 0), "instance.parameters should have 0 keys."
     
     def test_constructionWithParameters(self):
         params = {"tagged":"ptvs", "pagesize":"1"}
-        instance = core.Parameters(params)
+        instance = separams.Parameters(params)
         self.assertEqual(instance.getSize(), 2), "instance.parameters should have 2 keys."
 
     def test_addParameter(self):
         params = {"tagged":"ptvs", "pagesize":"1"}
-        instance = core.Parameters(params)
+        instance = separams.Parameters(params)
         instance.addParameter("site", "stackoverflow")
         self.assertEqual(instance.getSize(), 3), "instance.parameters should have 3 keys."
         self.assertDictContainsSubset({"site": "stackoverflow"}, instance.get())
 
     def test_tagCount(self):
         params = {"tagged":"ptvs;ironpython"}
-        instance = core.Parameters(params)
+        instance = separams.Parameters(params)
         self.assertEqual(instance._tagCount, 2)
         instance.addTag("debug")
         self.assertEqual(instance._tagCount, 3)
@@ -44,14 +59,14 @@ class Test_Core(unittest.TestCase):
     
     def test_getParameter(self):
         params = {"tagged":"ptvs"}
-        instance = core.Parameters(params)
+        instance = separams.Parameters(params)
         self.assertEquals("ptvs", instance.getParameter("tagged"))
         self.assertFalse(instance.getParameter("site"))
 
 
     def test_addTag(self):
         params = {"tagged":"ptvs"}
-        instance = core.Parameters(params)
+        instance = separams.Parameters(params)
         instance.addTag("ironpython")
         self.assertNotIn("django", instance.getParameter("tagged"))
         self.assertIn("ironpython", instance.getParameter("tagged"))
@@ -62,7 +77,7 @@ class Test_Core(unittest.TestCase):
         self.assertFalse(instance.addTag("sixth")) # core.MAX_TAGS currently set to 5
 
     def test_getAsQueryString(self):
-        instance = core.Parameters()
+        instance = separams.Parameters()
         self.assertFalse(bool(instance.getAsQueryString())) # Query string should be the empty string, ''.
         instance.addParameter("tagged", "ptvs")
         self.assertEqual("tagged=ptvs", instance.getAsQueryString())
@@ -73,7 +88,7 @@ class Test_Core(unittest.TestCase):
 
     def test_removeTag(self):
         params = {'tagged':'ptvs;python;debug'}
-        instance = core.Parameters(params)
+        instance = separams.Parameters(params)
         instance.removeTag('ptvs')
         self.assertEqual(instance.getParameter('tagged'), 'python;debug')
         instance.removeTag('python')
---
+++ Minerva/owl/stackexchangequery.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+from owl import stackexchangeparameters as separams
+
+
+class Query(object):
+    BASE_URL = 'https://api.stackexchange.com'
+    VERSION = '2.2'
+    VALID_QUERY_TYPES = ['questions']   # Only add valid query types as they are implemented in Minerva.
+    VALID_SITES = ['stackoverflow'] # Could potentially add additional sites without any additional implementation.
+
+    def __init__(self, site, params = None):
+        self.startURL = Query.BASE_URL + "/" + Query.VERSION
+        self.parameters = separams.Parameters(params)
+        self.setQueryType('questions')
+        self.setSite(site)
+    
+    def buildQueryString(self):
+        return self.startURL + '/' + self.getQueryType() + '?' + self.parameters.getAsQueryString()
+
+    def setQueryType(self, type):
+        if not type in Query.VALID_QUERY_TYPES:
+            raise ValueError("%s as a query type is not implemented." % type)
+        self._queryType = type
+
+    def getQueryType(self):
+        return self._queryType
+
+    def setSite(self, site):
+        if not site in Query.VALID_SITES:
+            raise ValueError("%s is not a valid StackExchange site or its querying is not implemented in Minerva." % site)
+        self._site = site
+
+    def getSite(self):
+        return self._site
+
+
+# Here for testing/debugging.
+params = {"tagged":"ptvs;python"}
+instance = Query('stackoverflow', params)
+print(instance.buildQueryString())
