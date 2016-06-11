6d3855386e0abdd3ea8fa2dc2b3ca2de8244d8f2
Alex Neuenkirk <alex.neuenkirk@gmail.com>
2016-06-10 22:12:16 -0700

    Started implementing methods for class UrlRequester.

--- Minerva/spider/webrequest.py
+++ Minerva/spider/webrequest.py
@@ -1,35 +1,55 @@
 #!usr/bin/env python
-try:
-    import urllib
-    httpGet = urllib.request.urlopen
-except ImportError:
-    import urllib2
-    httpGet = urllib2.urlopen
+# Special thanks to Lucas Jones (lucjon on github) for providing a portion of the send method's code.  Find it online at https://github.com/lucjon/Py-StackExchange/blob/master/stackexchange/web.py.
+import urllib.request
 
 
-class Request(object):
+class UrlRequester(object):
 
-    def __init__(self):
-        pass
+    def __init__(self, url):
+        self.setUrl(url)
+        self._cache = {}
 
     def setUrl(self, url):
-        pass
+        self._url = url
+        return True if url else False   # Allow quick error check.
 
     def getUrl(self):
-        pass
+        return self._url
 
     def send(self):
-        pass
+        # Build the request.
+        req = urllib.request.Request(self.getUrl())
+        req.add_header('Accept-Encoding', 'gzip')
+        req_open = urllib.request.build_opener()
+
+        # Make the request.
+        try:
+            conn = req_open.open(req)
+            info = conn.info()
+            data = conn.read()
+            error_code = 200
+        except urllib.error.HTTPError as e:
+            error_code = e.code
+            # A hack (header's are undocumented property), but there's a sensible way to get them.
+            info = getattr(e, 'headers', {})
+            req_data = e.read()
+
+        # Decompress it, if necessary.
+
+        # Check for errors.
+
+        # Cache it.
+            #self._cache(req_object)
 
     def read(self):
         pass
 
-    def _log(self):
+    def __log(self):
         """Log a number of items about this query."""
         # tags, query - should we cache as well
-        pass
+        raise NotImplementedError
 
-    def _cache(self):
+    def __cache(self):
         raise NotImplementedError
 
 
