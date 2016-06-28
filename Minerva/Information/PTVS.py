110f90974d609c1263ba07a5c57428ac6c54f281
Alex Neuenkirk <t-alneue@microsoft.com>
2016-06-28 15:06:23 -0700

    Continued work on getHelp.

---
+++ Minerva/Information/PTVS.py
@@ -0,0 +1,106 @@
+LINKS = {
+    "WIKI": "https://github.com/Microsoft/PTVS/wiki",
+    "Installation": "https://github.com/Microsoft/PTVS/wiki/PTVS-Installation",
+    "Feature Matrix": "https://github.com/Microsoft/PTVS/wiki/Features-Matrix",
+    "Overview Videos": "https://github.com/Microsoft/PTVS/wiki/Videos",
+    "Contributing": "https://github.com/Microsoft/PTVS/wiki/Contributing-to-PTVS",
+    "Build Instructions": "https://github.com/Microsoft/PTVS/wiki/Build-Instructions-for-PTVS",
+    "Tutorials": {
+        "Bottle and Azure Table Storage": "https://github.com/Microsoft/PTVS/wiki/Bottle-and-Azure-Table-Storage-on-Azure",
+        "Bottle and MongoDB": "https://github.com/Microsoft/PTVS/wiki/Bottle-and-MongoDB-on-Azure",
+        "Flask and Azure Table Storage": "https://github.com/Microsoft/PTVS/wiki/Flask-and-Azure-Table-Storage-on-Azure",
+        "Flask and MongoDB": "https://github.com/Microsoft/PTVS/wiki/Flask-and-MongoDB-on-Azure",
+        "Django and SQL Database": "https://github.com/Microsoft/PTVS/wiki/Django-and-SQL-Database-on-Azure",
+        "Django and MySQL": "https://github.com/Microsoft/PTVS/wiki/Django-and-MySQL-on-Azure"
+    },
+    "Editing": "https://github.com/Microsoft/PTVS/wiki/Editor-Features",
+    "Intellisense": "https://github.com/Microsoft/PTVS/wiki/Editor-Features#intellisense",
+    "Navigation": "https://github.com/Microsoft/PTVS/wiki/Editor-Features#navigation",
+    "Code Formatting": "https://github.com/Microsoft/PTVS/wiki/Code-Formatting",
+    "Refactoring": {
+        "BASE_URL": "https://github.com/Microsoft/PTVS/wiki/Refactoring",
+        "Rename": "https://github.com/Microsoft/PTVS/wiki/Refactoring#rename-variable",
+        "Extract Method": "https://github.com/Microsoft/PTVS/wiki/Refactoring#extract-method",
+        "Add Import": "https://github.com/Microsoft/PTVS/wiki/Refactoring#add-import",
+        "Remove Imports": "https://github.com/Microsoft/PTVS/wiki/Refactoring#remove-imports"
+    },
+    "Interactive REPL": {
+        "BASE_URL": "https://github.com/Microsoft/PTVS/wiki/Interactive-REPL",
+        "Switching Scopes": "https://github.com/Microsoft/PTVS/wiki/Interactive-REPL#switching-scopes",
+        "Send Code to Interactive": "https://github.com/Microsoft/PTVS/wiki/Interactive-REPL#sending-code-to-interactive",
+        "Using IPython with PTVS": "https://github.com/Microsoft/PTVS/wiki/Using-IPython-with-PTVS"
+    },
+    "Debugging": {
+        "BASE_URL": "https://github.com/Microsoft/PTVS/wiki/Debugging",
+        "Basic Debugging": "https://github.com/Microsoft/PTVS/wiki/Debugging#basic-debugging",
+        "Debugging with a Project": "https://github.com/Microsoft/PTVS/wiki/Debugging#debugging-with-a-project",
+        "Debugging without a Project": "https://github.com/Microsoft/PTVS/wiki/Debugging#debugging-without-a-project",
+        "The Debug Interactive Window": "https://github.com/Microsoft/PTVS/wiki/Debugging#the-debug-interactive-window",
+        "Remote Debugging": {
+            "Windows, Linux, and OS X": "https://github.com/Microsoft/PTVS/wiki/Cross-Platform-Remote-Debugging",
+            "Microsoft Azure Web Sites": "https://github.com/Microsoft/PTVS/wiki/Azure-Remote-Debugging"
+        },
+        "Mixed-Mode Debugging": "https://github.com/Microsoft/PTVS/wiki/Mixed-Mode-Debugging"
+    },
+    "Profiling": "https://github.com/Microsoft/PTVS/wiki/Profiling",
+    "Projects": {
+        "BASE_URL": "https://github.com/Microsoft/PTVS/wiki/Projects",
+        "Project Types": "https://github.com/Microsoft/PTVS/wiki/Projects#project-types",
+        "Django": "https://github.com/Microsoft/PTVS/wiki/Django",
+        "Web Projects": "https://github.com/Microsoft/PTVS/wiki/Web-Project",
+        "Cloud Service Project": "https://github.com/Microsoft/PTVS/wiki/Cloud-Project",
+        "Working without Projects": "https://github.com/Microsoft/PTVS/wiki/Projects#lightweight-usage-project-free",
+        "New Project from Existing Code": "https://github.com/Microsoft/PTVS/wiki/Projects#create-project-from-existing-files",
+        "Linked Files": "https://github.com/Microsoft/PTVS/wiki/Projects#linked-files",
+        "Search Paths": "https://github.com/Microsoft/PTVS/wiki/Projects#search-paths",
+        "References": "https://github.com/Microsoft/PTVS/wiki/Projects#references",
+        "Python Environments": "https://github.com/Microsoft/PTVS/wiki/Python-Environments",
+        "Managing Requirements": "https://github.com/Microsoft/PTVS/wiki/Python-Environments#managing-required-packages"
+    },
+    "Unit Tests": "https://github.com/Microsoft/PTVS/wiki/Unit-Tests",
+    "PyLint": "https://github.com/Microsoft/PTVS/wiki/PyLint",
+    "Azure SDK for Python": "https://github.com/Microsoft/PTVS/wiki/AzureSDK",
+    "Kinect for Python": "https://github.com/Microsoft/PTVS/wiki/PyKinect",
+    "wfastcgi (on PyPI)": "https://pypi.python.org/pypi/wfastcgi"
+}
+
+KEY_MAP = {
+    "Debugging": {
+        'synonyms': ['debug', 'debugger', 'debugging', 'debuggor'],
+        'keywords': {
+            'Remote Debugging': ['remote', 'attach', 'attached', 'nonlocal']
+        }
+    }
+}
+
+def literalToKey(literal):
+    """Returns the appropriate key value for links when given a literal.
+    For example, if the literal passed in is "debug" it will map this to
+    "Debugging" for extraction of information out of LINKS.
+    """
+    for k, v in KEY_MAP.items():
+        if literal.lower() in v['synonyms']:
+            return k
+    return 'WIKI'
+
+def filterWithKeyword(feature, keyword):
+    """Determine if a previously identified feature has more specific 
+    context in relation to 'keyword'.  Will return a tuple of keys for
+    which to extract a link from LINKS.
+    """
+    keywords = KEY_MAP[feature]['keywords']
+    for k, v in keywords.items():
+        if keyword in v:
+            return k
+    return None
+
+def getRefinedKeys(keyFeature, keywords):
+    """Returns a list of refined key features, based on any keywords that were
+    found in the original query.
+    """
+    if 0 < len(keywords):
+        refinedKeys = []
+        for word in keywords:
+            filtered = filterWithKeyword(keyFeature, word)
+            if filtered: refinedKeys.append(filtered)
+    return refinedKeys if len(refinedKeys) > 0 else None
--- Minerva/luis/LuisInterpreter.py
+++ Minerva/luis/LuisInterpreter.py
@@ -1,5 +1,6 @@
 from abc import ABCMeta, abstractmethod
 from Essentials import enterAndExitLabels
+import PTVS
 
 class AbstractLuisInterpreter(object):
     """An interface for creating extension specific interpreters for Visual Studio."""
@@ -9,42 +10,100 @@ class AbstractLuisInterpreter(object):
     def analyze(self, json):
         pass
 
-
 class BaseLuisInterpreter(AbstractLuisInterpreter):
     """A base class for all interpreters."""
     def __init__(self, **kwargs):
         return super().__init__(**kwargs)
 
-    def analyze(self):
-        """Analyzes the json returned from a call to the base LuisClient class's method, query_raw."""
-        # Code to analyze goes here.
-        raise NotImplementedError
-
-
-class PythonLuisInterpreter(AbstractLuisInterpreter):
-    """Interprets quetions for the Python Tools for Visual Studio help bot."""
-
-    # Map an intent to a function.
-    def __init__(self):
-        self._STRATAGIES = {
-            'Get Help': self.__getHelp,
-            'undefined': self.__undefined
-        }
-
     def analyze(self, json):
-        intent = self.__getTopScoringIntent(json)
-        return self._STRATAGIES[intent](json)
+        """Analyzes the json returned from a call to the base LuisClient class's method, query_raw."""
+        raise NotImplementedError("Function analyze has not yet been customized.")
 
-    def __getTopScoringIntent(self, json):
+    @classmethod
+    def getTopScoringIntent(cls, json):
         try:
             return json['intents'][0]['intent']
-        except KeyError:
+        except LookupError:
             return 'undefined'
 
-    def __getHelp(cls, json):
-        return "I will be happy to help."
+    @classmethod
+    def getLiterals(cls, json):
+        return [e['entity'] for e in json['entities']]
 
-    def __undefined(cls):
+    @classmethod
+    def getTypes(cls, json):
+        return [e['type'] for e in json['entities']]
+
+    @classmethod
+    def getAllLiteralsOfType(cls, type, json):
+        return [e['entity'] for e in json['entities'] if e['type'] == type]
+
+class PythonLuisInterpreter(BaseLuisInterpreter):
+    """Interprets quetions for the Python Tools for Visual Studio help bot."""
+
+    def __init__(self):
+        # Map an intent to a function.
+        self._STRATAGIES = {
+            'Get Help': self._getHelp,
+            'undefined': self._undefined
+        }
+        self.response = ""
+
+    def analyze(self, json):
+        """Analyzes the json returned from a call to LuisClient's method, query_raw."""
+        self.response = ""
+        intent = self.getTopScoringIntent(json)
+        return self._STRATAGIES[intent](json)
+
+    def _getHelp(self, json):
+        """Called from function 'analyze' when the intent of a LUIS query is determined
+        to be 'Get Help'.
+        """
+        literals = self.getLiterals(json)
+        types = self.getTypes(json)
+        keywords = self.getAllLiteralsOfType('Keyword', json)
+        for i in range(len(literals)):
+            print("%s - %s" % (types[i], literals[i]))
+
+        if "Visual Studio Feature" in types:
+            self.evaluateFeatureQuery(literals, types, keywords)
+
+    def evaluateFeatureQuery(self, literals, types, keywords):
+        """Handles the processing of a query that contains a Visual Studio Feature entity.
+        """
+        feature = self.literalFromType("Visual Studio Feature", literals, types)
+        keyFeature = PTVS.literalToKey(feature)
+        if keyFeature:
+            print("I can definitely help you with {0}.\n".format(keyFeature.lower()))
+            # Evaluate the keywords for a more defined feature.
+            refinedKeys = PTVS.getRefinedKeys(keyFeature, keywords)
+        if refinedKeys:
+            for index, value in enumerate(refinedKeys):
+                r = input("Are you asking about {0}? Y/N".format(value))
+                if 'Y' == r.upper():
+                     refined = PTVS.LINKS[keyFeature][value]
+        if refined:
+            if type(refined) is dict:
+                keys = refined.keys()
+                items = refined.values()
+                print("Okay, which is more pertinent to what you're looking for?")
+                for i in range(len(items)):
+                    print("{0}: {1}".format(i + 1, items[i]))
+                index = input()
+                try:
+                    index = int(index)
+                    print("I suggest you visit this site: {0}".format(refined[keys[index]]))
+                except IndexError:
+                    print("That was not a valid selection.")
+
+
+
+    def literalFromType(self, type, literals, types):
+        """Returns the literal string for the first of 'type' found within types."""
+        i = types.index(type)
+        return literals[i]
+
+    def _undefined(self):
         return "I'm sorry, I don't know what you're asking."
 
 
