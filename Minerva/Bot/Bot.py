58c6e000f901d9e33a3e6217bfbeb2ac8d9dcbfc
Alex Neuenkirk <alex.neuenkirk@gmail.com>
2016-07-06 17:54:32 -0700

    Transferred constant strings to their own module.

--- Minerva/Bot/Bot.py
+++ Minerva/Bot/Bot.py
@@ -1,35 +1,5 @@
 import random
-
-POSITIVE_ACKS = [
-    "I can definitely help you with {0}.",
-    "I will be happy to assist with {0}.",
-    "Sure, I can help you with {0}."
-]
-
-SUGGEST_URL = [
-    "The following is a good resource: {0}.",
-    "I suggest you visit: {0}.",
-    "You'll find information on that here: {0}."
-]
-
-CLARIFYING = [
-    "Which of these is more pertinent in your case?",
-    "Which one of these best describes what you need?"
-]
-
-START = [
-    "What can I help you with?",
-    "How can I help you?",
-    "What can I do for you?"
-]
-
-ALL_STRINGS = {
-    'positive_acks': POSITIVE_ACKS,
-    'suggest_url': SUGGEST_URL,
-    'clarify': CLARIFYING,
-    'start': START
-}
-
+from DialogueStrings import ALL_STRINGS
 
 class Bot(object):
     """Communicates with the user.
