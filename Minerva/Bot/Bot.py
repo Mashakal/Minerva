1a8e9a7217b5d0f3a2bd335579576c2ad30ac400
Alex Neuenkirk <alex.neuenkirk@gmail.com>
2016-07-06 17:00:06 -0700

    Made get_random_string_constant protected.  Moved clarify from VSBot to Bot.

--- Minerva/Bot/Bot.py
+++ Minerva/Bot/Bot.py
@@ -34,7 +34,7 @@ ALL_STRINGS = {
 class Bot(object):
     """Communicates with the user.
     """
-    def get_random_string_constant(self, type):
+    def _get_random_string_constant(self, type):
         """Grabs an array of strings from the ALL_STRINGS dictionary.
         Returns a random string from the extracted array.
         """
@@ -66,30 +66,21 @@ class Bot(object):
         print(s)
         return self._prompt()
 
-    def acknowledge(self, s, positive = True):
+    def acknowledge(self, s, positive=True):
         """Output a positive or negative acknowledgement, formatted
         with s.
         """
         t = 'positive_acks' if positive else 'negative_acks'
-        r = self.get_random_string_constant(t)
+        r = self._get_random_string_constant(t)
         self.say(r.format(s))
-
-class VSBot(Bot):
-    """A Visual Studio bot.
-    """
-    def suggest_url(self, url):
-        """Output a message suggesting the user visit url.
-        """
-        r = self.get_random_string_constant('suggest_url')
-        self.say(r.format(url))
 
     def clarify(self, opts, msg=None, indent=2):
         """Outputs a clarifying message to standard output followed by an 
         ordered list of options defined by opts.  Ordering starts at 1 and 
         each element of opts is output on a new line.  Returns the opt chosen 
         by the user.  The message can be set explicitly via msg.  Indent can 
-        be set explicitly to change the number of spaces before an opt is 
-        written to standard output.
+        be set explicitly to change the number of empty spaces precending each
+        opt.
         """
         def _validate_input(input, opts):
             """A helper function for clarify.  Checks that a user has entered
@@ -103,7 +94,7 @@ class VSBot(Bot):
                 return False
             return True
 
-        m = msg if msg else self.get_random_string_constant('clarify')
+        m = msg if msg else self._get_random_string_constant('clarify')
         self.say(m)
         for i, v in enumerate(opts):
             print(" " * indent + "{0}: {1}".format(i + 1, v))
@@ -112,6 +103,15 @@ class VSBot(Bot):
             i = self.ask("{0} is not valid.  Enter a valid choice.".format(i))
         return opts[int(i) - 1]
 
+class VSBot(Bot):
+    """A Visual Studio bot.
+    """
+    def suggest_url(self, url):
+        """Output a message suggesting the user visit url.
+        """
+        r = self._get_random_string_constant('suggest_url')
+        self.say(r.format(url))
+
     def start_query(self, msg=None):
-        m = msg if msg else self.get_random_string_constant('start')
+        m = msg if msg else self._get_random_string_constant('start')
         return self.ask(m)
