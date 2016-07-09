c96965fd4021d3f277d69fe1fbc496c21ca575b6
Alex Neuenkirk <alex.neuenkirk@gmail.com>
2016-07-09 09:47:55 -0700

    Updated to meet Agent.py requirements.

--- Minerva/Bot/DialogueStrings.py
+++ Minerva/Bot/DialogueStrings.py
@@ -4,13 +4,17 @@ POSITIVE_ACKS = [
     "Sure, I can help you with {0}."
 ]
 
+NEGATIVE_ACKS = [
+    "Hmm, I don't think that has antyhing to do with visual studio.  Let me see what I can find on StackOverflow."
+]
+
 SUGGEST_URL = [
     "The following is a good resource: {0}.",
     "I suggest you visit: {0}.",
     "You'll find information on that here: {0}."
 ]
 
-CLARIFYING = [
+OPTIONS = [
     "Which of these is more pertinent in your case?",
     "Which one of these best describes what you need?"
 ]
@@ -21,9 +25,16 @@ START = [
     "What can I do for you?"
 ]
 
+CLARIFY = [
+    "Are you asking about"
+    #"Which is your main point of interest "
+]
+
 ALL_STRINGS = {
     'positive_acks': POSITIVE_ACKS,
     'suggest_url': SUGGEST_URL,
-    'clarify': CLARIFYING,
-    'start': START
+    'clarify': CLARIFY,
+    'start': START,
+    'options': OPTIONS,
+    'negative_acks': NEGATIVE_ACKS
 }
