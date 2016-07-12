42e12d3552879b7feb238c3afb88e9f4b34c6726
Alex Neuenkirk <alex.neuenkirk@gmail.com>
2016-07-11 17:09:55 -0700

    Changed all triggers lists to sets.  Added more info to KEY_MAP.

--- Minerva/Information/PTVS.py
+++ Minerva/Information/PTVS.py
@@ -14,7 +14,7 @@ LINKS = {
     "Installation": "https://github.com/Microsoft/PTVS/wiki/PTVS-Installation",
     "Feature Matrix": "https://github.com/Microsoft/PTVS/wiki/Features-Matrix",
     "Overview Videos": "https://github.com/Microsoft/PTVS/wiki/Videos",
-    "Contributing": "https://github.com/Microsoft/PTVS/wiki/Contributing-to-PTVS",
+    "Contributing": "https://github.com/Microsoft/PTVS/wiki/Contributing-to-PTVS,",
     "Build Instructions": "https://github.com/Microsoft/PTVS/wiki/Build-Instructions-for-PTVS",
     "Tutorials": {
         "Bottle and Azure Table Storage": "https://github.com/Microsoft/PTVS/wiki/Bottle-and-Azure-Table-Storage-on-Azure",
@@ -76,49 +76,124 @@ LINKS = {
 }
 
 KEY_MAP = {
+    'Tutorials': {
+        'Triggers': {'tutorials', 'tutorial', 'show me'},
+        'Bottle and Azure Table Storage': {
+            'Triggers': {'bottle and azure table storage', 'bottle and azure', 'bottle table storage'}
+        },
+        'Bottle and MongoDB': {
+            'Triggers': {'bottle and mongodb', 'mongodb and bottle'}
+        },
+        'Flask and Azure Table Storage': {
+            'Triggers': {'flask and azure table storage', 'flask and azure', 'flask table storage'}
+        },
+        'Flask and MongoDB': {
+            'Triggers': {'flask and mongodb', 'mongodb and flask'}
+        },
+        'Django and SQL Database': {
+            'Triggers': {'django and sql database', 'django and sql', 'django database', 'django with sql'}
+        },
+        'Django and MySql': {
+            'Triggers': {'django and mysql', 'django with mysql'}
+        }
+    },
+    'WIKI': {
+        'Triggers': {'wiki'}
+    },
     "Installation": {
-        "Triggers": ['installation', 'install', 'instalation', 'installs']
+        "Triggers": {'installation', 'install', 'instalation', 'installs'}
     },
     'Projects': {
-        'Triggers': ['projects', 'project'],
+        'Triggers': {'projects', 'project'},
         "Django": {
-            'Triggers': ['django']
+            'Triggers': {'django', 'django web project', 'django project'}
         },
         'Web Projects': {
-            'Triggers': ['web projects', 'web']
+            'Triggers': {'web projects', 'web'}
         },
         'Project Types': {
-            'Triggers': ['project types', 'types', 'type']
+            'Triggers': {'project types', 'types', 'type'}
         },
         'Cloud Service Project': {
-            'Triggers': ['cloud service project', 'cloud project', 'cloud']
+            'Triggers': {'cloud service project', 'cloud project', 'cloud'}
         },
         'Working Without Projects': {
-            'Triggers': ['working without projects', 'without projects', 'lightweight', 'lightweight-usage', 'project-free', 'project free']
+            'Triggers': {'working without projects', 'without projects', 'lightweight', 'lightweight-usage', 'project-free', 'project free'}
+        },
+        'New Project from Existing Code':{
+            'Triggers': {'new project', 'existing code', 'create project', 'create new project'}
+        },
+        'Linked Files': {
+            'Triggers': {'linked files', 'linked', 'linked file', 'create a linked file', 'link a file', 'link'}
+        },
+        'Search Paths': {
+            'Triggers': {'search paths', 'user defined modules', 'user defined packages'}
+        },
+        'References': {
+            'Triggers': {'references', 'what are references'}
+        },
+        'Python Environments': {
+            'Triggers': {'python environments', 'environments', 'help with environments'}
+        },
+        'Managing Requirements': {
+            'Triggers': {'managing requirements', 'requirements', 'how to use requirements'}
+        }
+    },
+    'Navigation': {
+        'Triggers': {'navigation', 'navigate'},
+    },
+    'Refactoring': {
+        'Triggers': {'refactoring'},
+        'Rename': {
+            'Triggers': {'rename', 'rename a variable', 'rename something', 'rename a symbol', 'symbol'}
+        },
+        'Extract Method': {
+            'Triggers': {'extract method', 'extract'}
+        },
+        'Add Import': {
+            'Triggers': {'add import', 'add imports'}
+        },
+        'Remove Imports': {
+            'Triggers': {'remove imports', 'remove import'}
         }
     },
     "Debugging": {
-        'Triggers': ['debugging', 'debug', 'debugger', 'debuggor'],
+        'Triggers': {'debugging', 'debug', 'debugger', 'debuggor'},
         'Remote Debugging': {
-            'Triggers': ['remote', 'attach', 'attached', 'nonlocal', 'ptvsd']
+            'Triggers': {'remote', 'attach', 'attached', 'nonlocal', 'ptvsd'}
+        }
+    },
+    'Interactive REPL': {
+        'Triggers': {'interactive repl', 'repl', 'interactive'},
+        'Switching Scopes': {
+            'Triggers': {'switching scopes'}
+        },
+        'Send Code to Interactive': {
+            'Triggers': {'send code to interactive', 'send to interactive', 'to interactive'}
+        },
+        'Using IPython with PTVS': {
+            'Triggers': {'using ipython with ptvs', 'using ipython', 'ipython'}
         }
     },
     "Feature Matrix": {
-        'Triggers': ['feature matrix', 'features']
+        'Triggers': {'feature matrix', 'features'}
     },
     'Overview Videos': {
-        'Triggers': ['overview videos', 'video', 'videos', 'overview']
+        'Triggers': {'overview videos', 'video', 'videos', 'overview'}
     },
     'Contributing': {
-        'Triggers': ['contributing', 'contribute', 'pull request']
+        'Triggers': {'contributing', 'contribute', 'pull request'}
     },
     'Build Instructions': {
-        'Triggers': ['build instructions', 'building', 'build']
+        'Triggers': {'build instructions', 'building', 'build'}
     },
     'Editing': {
-        'Triggers': ['editing', 'code editing']
+        'Triggers': {'editing', 'code editing'}
     },
     'Intellisense': {
-        'Triggers': ['intellisense', 'autocomplete']
+        'Triggers': {'intellisense', 'autocomplete', 'completions', 'signature help', 'quick info', 'completion'}
+    },
+    'Code Formatting': {
+        'Triggers': {'code formatting'}
     }
 }
