c8a7cc9606f81ef34ba66f91bd136a697b28f715
Alex Neuenkirk <t-alneue@microsoft.com>
2016-07-15 09:31:33 -0700

    Started implementing Brett's suggestions.

--- Minerva/luis/LuisInterpreter.py
+++ Minerva/luis/LuisInterpreter.py
@@ -1,20 +1,23 @@
+import sys
+import abc
+
 import InfoManager
 import collections
 
 # For development purposes only:
 from Essentials import enter_and_exit_labels, print_smart
-import sys
+
 
 # Constants
 # These may be better off in the Bot module.
 _YES_WORDS = ['yes', 'yeah', 'okay', 'ok', 'k', 'y', 'ya', 'right', 'correct', "that's right", 'sure', 'for sure']
 _NO_WORDS = ['no', 'n', 'nah', 'nope', 'negative']
 
-class BaseLuisInterpreter(object):
-    """A base class for all interpreters."""
-    def __init__(self, **kwargs):
-        return super().__init__(**kwargs)
 
+class BaseLuisInterpreter(abc.ABC):
+    """A base class for all interpreters."""
+    
+    @abc.abstractmethod
     def analyze(self, json):
         """Analyzes the json returned from a call to the base LuisClient class's method, query_raw."""
         raise NotImplementedError("Function analyze has not yet been customized.")
@@ -26,25 +29,31 @@ class BaseLuisInterpreter(object):
             return 'None'
 
     def _get_literals(self, json):
-        return set(([e['entity'] for e in json['entities']]))
+        return {e['entity'] for e in json['entities']}
 
     def _get_types(self, json):
-        return set(([e['type'] for e in json['entities']]))
+        return {e['type'] for e in json['entities']}
 
     def _literals_given_type(self, t, json):
-        return set(([e['entity'] for e in json['entities'] if e['type'] == t]))
+        return {e['entity'] for e in json['entities'] if e['type'] == t}
 
     def _literals_given_parent_type(self, parent, json):
-        return set(([child['entity'] for child in json['entities'] if parent in child['type']]))
+        return {child['entity'] for child in json['entities'] if parent in child['type']}
+
+    def _print_from_data(self, data):
+        #for k, v in data.items():
+        #    print("{0}: {1}".format(k.upper(), v))
+        for key in ['query', 'intent', 'phrase_jargon', 'single_jargon', 'auxiliaries', 'subjects']:
+            print ("  {0}:\t{1}".format(key.upper(), data[key]))
+        print()
+
 
 class ProjectSystemLuisInterpreter(BaseLuisInterpreter):
-    """Interprets questions for language specific project systems of Visual Studio
-    as a part of a help bot.
-    """
+    """Interprets questions for language specific project systems of Visual Studio."""
+
     def __init__(self, bot, project_system):
         # _info is the main point of access for anything specific to a project
         self._info = InfoManager.ProjectSystemInfoManager(project_system)
-
         # Use _bot to interact with the user (e.g. ask a question, clarify between options, acknowledge keywords).
         self._bot = bot
         
@@ -67,14 +76,8 @@ class ProjectSystemLuisInterpreter(BaseLuisInterpreter):
         finally:
             func(data)
 
-    # Utility functions.
-    def _print_from_data(self, data):
-        #for k, v in data.items():
-        #    print("{0}: {1}".format(k.upper(), v))
-        for key in ['query', 'intent', 'phrase_jargon', 'single_jargon', 'auxiliaries', 'subjects']:
-            print ("  {0}:\t{1}".format(key.upper(), data[key]))
-        print()
 
+    # Utility functions.
     def _format_data(self, json):
         """Formats the raw json into a more easily managable dictionary."""
         return {
@@ -83,7 +86,7 @@ class ProjectSystemLuisInterpreter(BaseLuisInterpreter):
             'entities': json['entities'],
         # Leaf keys - these point to a value or some container of values.
             'query': json['query'],
-            'intent': json['intents'][0]['intent'],
+            'intent': self._get_top_scoring_intent(json),
             'other_intents': json['intents'][1:],
             'subjects': self._literals_given_type('Subject', json),
             'auxiliaries': self._literals_given_type('Auxiliary', json),
@@ -96,7 +99,7 @@ class ProjectSystemLuisInterpreter(BaseLuisInterpreter):
             'solve_problem_triggers': self._literals_given_parent_type('Solve Problem Triggers::', json)
         }
 
-    def __get_paths(self, word_set):
+    def _get_paths(self, word_set):
         # I die a little inside everytime I look at this function.
         """Get's paths to all words in the set, if a path for it exists.
         Filters the paths found such that only the deepest path will be
@@ -105,8 +108,8 @@ class ProjectSystemLuisInterpreter(BaseLuisInterpreter):
         same query.
         """
         def get_paths(word_set):
-            """A helper function for __get_paths.  Returns an unfiltered list
-            of all the paths pointed to by words in the word set.
+            """Returns an unfiltered list of all the paths pointed to by 
+            words in the word set.
             """
             paths = []
             for word in word_set:
@@ -117,7 +120,6 @@ class ProjectSystemLuisInterpreter(BaseLuisInterpreter):
         
         def remove_duplicates(paths, key):
             """Remove all but the longest path from paths."""
-            list_max = None     # The list with the longest length.
             # Get the paths that contain key.
             with_key = [path for path in paths if key in path]
             # Find the longest one.
@@ -130,17 +132,14 @@ class ProjectSystemLuisInterpreter(BaseLuisInterpreter):
         
         paths = get_paths(word_set)
         flattened_paths = [p for path in paths for p in path]
-        counts = {}
-        counter = collections.Counter()
-        for key in flattened_paths:
-            counter[key] += 1
+        counter = collections.Counter(flattened_paths)
         for key, count in counter.most_common(): # Get ALL elements in counter.
             if count > 1:
                 paths = remove_duplicates(paths, key) 
         # TODO:  Log how many paths were returned, and which ones.
         return paths
 
-    def __get_all_paths(self, interests, data):
+    def _get_all_paths(self, interests, data):
         """Returns a dictionary where the interest is the key and a list of
         keys that map to a url is the value.  If no path is found for any
         interest, its value will be None.
@@ -153,19 +152,19 @@ class ProjectSystemLuisInterpreter(BaseLuisInterpreter):
             all_paths[interest] = path if path else None
         return all_paths
 
-    def __topic_from_path(self, path):
+    def _topic_from_path(self, path):
         """Returns the 'topic' of a path, that is, the last key in the list of
         keys in the path."""
         return path[len(path) - 1]
 
-    def __longest_paths(self, paths):
+    def _longest_paths(self, paths):
         max_len = 0
         for path in paths:
             if len(path) > max_len:
                 max_len = len(path)
         return [path for path in paths if len(path) == max_len]
 
-    def __complete_path(self, path):
+    def _complete_path(self, path):
         """If the given path does not already point to a string (we assume if it does the string is an url).
         It will ask the user to clarify the remaining topics until a string is reached.
         """
@@ -177,16 +176,18 @@ class ProjectSystemLuisInterpreter(BaseLuisInterpreter):
             end = end[choice]
         return path
       
+               
+    # Intent functions.
     def _learn_about_topic(self, data):
         """Another try at learn about topic procedure.
         """
         # Find all paths for any topic of interest found in the user's query.
         interests = ['phrase_jargon', 'single_jargon', 'auxiliaries', 'subjects']
-        paths_dict = self.__get_all_paths(interests, data)  # Dictionary - interest: paths
+        paths_dict = self._get_all_paths(interests, data)  # Dictionary - interest: paths
         all_paths = self._info.remove_subpaths(paths_dict)  # List of all paths, subpaths removed.
 
         # Get all of the deepest paths. THIS IS AN AREA WHOSE LOGIC CAN BE IMPROVED.
-        longest_paths = self.__longest_paths(all_paths)
+        longest_paths = self._longest_paths(all_paths)
         if not longest_paths:
             # Check if we have anything to go on.
             print("Luis didn't find any keywords in your query.  Sorry.")
@@ -194,20 +195,16 @@ class ProjectSystemLuisInterpreter(BaseLuisInterpreter):
             return    
 
         # Positive or negative acknowledgement.
-        topics = [self.__topic_from_path(path) for path in longest_paths]
+        topics = [self._topic_from_path(path) for path in longest_paths]
         self._bot.acknowledge(topics)
 
         # Map a topic to a corresponding url.
         urls = {topics[i]: self._info.get_url(path) for i, path in enumerate(longest_paths)}
         for i, path in enumerate(longest_paths):
-            path = self.__complete_path(path)
+            # Complete any path that doesn't point to an url.
+            path = self._complete_path(path)
             urls[topics[i]] = self._info.get_url(path)
 
-        # Make sure we have an url, otherwise we need some clarification.
-        #topics_needing_chosen = {k: v.keys() for k, v in urls.items() if not isinstance(v, str)}
-        #for t in topics_needing_chosen:
-        #    choice = self._bot.give_options(topics_needing_chosen[t])
-            
         # Suggest the url(s).
         self._bot.suggest_multiple_urls(list(urls.values()), list(urls.keys()))
 
@@ -215,97 +212,11 @@ class ProjectSystemLuisInterpreter(BaseLuisInterpreter):
 
         # Make additional action based on the feedback.
 
-    # Intent Functions.
-    #def _learn_about_topic(self, data):
-    #    """Called when the intent is determined to be 'Learn About Topic'.
-    #    """
-    #    def path_from_topic(paths, topic):
-    #        """Returns the first list in paths whose last element matches
-    #        topic.
-    #        """
-    #        for p in paths:
-    #            if p[len(p) - 1] == topic:
-    #                return p
-
-    #    self._bot.say("It looks like you want to learn about a topic.")
-    #    # Petricca may map a keyword to jargon or to auxiliaries, in most cases.
-    #    keywords = data['jargon'] | data['auxiliaries']
-    #    paths = self._info.get_paths(keywords)
-    #    matched_topics = [p[len(p) - 1] for p in paths]
-    #    print("Paths: {0}".format(paths))
-    #    print("Matched_topics: {0}".format(matched_topics))
-
-    #    if 1 < len(matched_topics):
-    #        topic = self._bot.give_options(matched_topics)
-    #        path = path_from_topic(topic)
-    #    elif 1 == len(matched_topics):
-    #        topic = paths[0][len(paths[0]) - 1]
-    #        path = paths[0]
-    #    else:
-    #        # Let's do something worthwhile here.
-    #        pass
-    #    self._bot.acknowledge(topic)
-    #    url = self._info.get_url(topic)
-    #    self._bot.suggest_url(url, topic)
-
-
-            
-
     def _solve_problem(self, data):
         """Called when the intent is determined to be 'Solve Problem'.
         """
         self._bot.say("It looks like you want to solve a problem.")
-
-    def _get_help(self, data):
-        """Called from function 'analyze' when the intent of a LUIS query is determined
-        to be 'Get Help'.
-        """
-        def clarify_paths(paths):
-            """Determine which topic is most pertinent to the user
-            when more than one unique path is found given the
-            user's query.
-            """
-            # One path is good, as long Luis picked up the right keywords.
-            if 1 == len(paths):
-                return paths
-            elif 1 < len(paths):
-                ending_keys = [p[len(p) - 1] for p in paths]
-                ans = self._bot.clarify(ending_keys)
-                return [p for p in paths for a in ans if p[len(p) - 1] == a]
-            else:
-                # No paths found.
-                return False
-
-        def get_ending_url(path):
-            """Given a single path, get to an url.
-            """
-            u = self._info.traverse_keys(path)
-            while not isinstance(u, str):   # Path might not lead to url yet.
-                # If our path doesn't point to a key with its own url,
-                # ask the user where to go from here.
-                keys = list(u.keys())
-                # We only need to ask when there is more than one potential key.
-                if 1 < len(keys):
-                    self._bot.acknowledge(path[len(path) - 1])
-                    next = self._bot.give_options([k for k in u.keys()])
-                    path.append(next)
-                else:
-                    path.append(keys[0])
-                u = self._info.traverse_keys(path)
-            return u
-
-        # Check if the user triggered any links to the wiki page.
-        paths = clarify_paths(data['paths'])
-        if paths:
-            urls = [get_ending_url(path) for path in paths]
-            topics = [self._info.get_url_description(u) for u in urls]
-            self._bot.acknowledge(topics)
-            self._bot.suggest_multiple_urls(urls, topics)
-        else:
-            # Try StackExchange
-            self._bot.say("Hmmm, I'm not sure the wiki can help.\nLet me see what I can find through stackoverflow.\n\n")
-            #raise NotImplementedError("Querying stackoverflow is not yet implemented.")
-            
+           
     def _none_intent(self):
         self._bot.say("I'm sorry, I don't know what you're asking.")
         
@@ -318,4 +229,4 @@ def main():
         print("{0}: {1}".format(path, trigger_paths[path]))
 
 if __name__ == "__main__":
-    sys.exit(int(main() or 0))
+    main()
