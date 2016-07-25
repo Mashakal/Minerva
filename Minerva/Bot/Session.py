1aa4dabbb9da24d62a6dc7091e94177868a30fad
Alex Neuenkirk <alex.neuenkirk@gmail.com>
2016-07-25 06:27:21 -0700

    Minor changes.

--- Minerva/Bot/Session.py
+++ Minerva/Bot/Session.py
@@ -8,18 +8,20 @@ import LuisClient
 import Essentials
 
 
-class Bot:
+class BotSession:
     pass
 
 
-class ProjectSystemBot(Bot):
-    """A bot for Visual Studio Project Systems."""
+class ProjectSystemBotSession(BotSession):
+
+    """Obtains a conversation's session data and state."""
 
     def __init__(self, project_system):
         self._luis_client = LuisClient.BotLuisClient()
-        self.agent = Agent.VSAgent()
+        self.agent = Agent.VSConsoleAgent()
         self.interpreter = LuisInterpreter.ProjectSystemLuisInterpreter(self.agent, project_system)
 
     def initiate_conversation(self):
         """Starts a conversation between the agent and the user."""
         return self.agent.start_query()
+
