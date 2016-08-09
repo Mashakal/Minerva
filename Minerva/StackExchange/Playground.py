21aaf4d2b2fb26d0f40509285018c2ddbbfd3a9c
Alex Neuenkirk <alex.neuenkirk@gmail.com>
2016-08-09 12:49:30 -0700

    Working on stackexchange queries.

--- Minerva/StackExchange/Playground.py
+++ Minerva/StackExchange/Playground.py
@@ -1,9 +1,9 @@
 import json
 
-import query
+import Query
 
 def main():
-    #mvqp = query.QueryParameter('tagged', ['ptvs'])
+    #mvqp = Query.QueryParameter('tagged', ['ptvs'])
     #print(mvqp.__str__())
     #for el in mvqp:
     #    print("Element is: {}".format(el))
@@ -12,15 +12,11 @@ def main():
     #mvqp.remove_value('ptvs')
     #print(str(mvqp))
 
-    #qs = query.StackExchangeQueryString()
-    #for o in qs.params: print(o)
-    
-    #print(str(qs))
-    #qs.add_param('tagged', 'ptvs')
-    #qs.add_param('tagged', 'debugging')
-    #qs.add_param('intitle', 'c#')
-    #print(str(qs))
-    #print(qs.get_full_string())
+
+    qs = Query.StackExchangeQueryString()
+    qs.add_param('tagged', 'ptvs')
+    qs.add_param('tagged', 'debugging')
+    qs.add_param('intitle', 'c#')
 
     #print("Remove params:")
     #print(str(qs))
@@ -37,18 +33,18 @@ def main():
     #print(qs.retrieve('site'))
     #print(str(qs))
 
-    q = query.StackExchangeQuery('stackoverflow')
-    q.set_query_path(query.QueryPaths.AdvancedSearch)
-    q.query_string.add_param('tagged', ['c #', 'python', 'debugging'])
-    q.query_string.add_param('filter', 'withbody')
-    q.query_string.add_param('pagesize', '20')
-    q.query_string.add_param('nottagged', 'pycharm')
-    print(q.query_string.__str__())
-    print(q.build_full_url())
+    #q = Query.StackExchangeQuery('stackoverflow')
+    #q.set_query_path(Query.QueryPaths.AdvancedSearch)
+    #q.query_string.add_param('tagged', ['c #', 'python', 'debugging'])
+    #q.query_string.add_param('filter', 'withbody')
+    #q.query_string.add_param('pagesize', '20')
+    #q.query_string.add_param('nottagged', 'pycharm')
+    #print(q.query_string.__str__())
+    #print(q.build_full_url())
     
-    q.go()
+    #q.initiate()
 
-    print(json.dumps(q.response._json, sort_keys=True, indent=4))
+    #print(json.dumps(q.response._json, sort_keys=True, indent=4))
     
     # Problems debugging c# and python at the same time in ptvs:
     # all_jargon: {'debugging'}
@@ -58,13 +54,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-#segments = {
-#    'scheme': 'https',
-#    'netloc': 'api.stackexchange.com',
-#    'path': '2.2/questions',
-#    'params': '',
-#    'query': 'order=desc&sort=activity&site=stackoverflow',
-#    'fragment': ''
-#}
