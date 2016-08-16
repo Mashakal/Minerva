f0a0cc603c6d862bbf0527c2354dcf1e189f2dbe
Alex Neuenkirk <alex.neuenkirk@gmail.com>
2016-08-16 08:44:58 -0700

    Adding deployment.

---
+++ Minerva/BotConnector/_deploy/deploy_credentials.py
@@ -0,0 +1,9 @@
+from azure.common.credentials import UserPassCredentials
+
+CREDENTIALS = UserPassCredentials(
+    "peon@pybotminervaoutlook.onmicrosoft.com",
+    "2301contoso@"
+)
+
+
+SUBSCRIPTION_ID = "94c6f711-4e62-4128-93ce-755657018c7a"
