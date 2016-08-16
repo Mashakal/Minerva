3aa257543003b9681f6868d1f4df9374b3f16701
Alex Neuenkirk <alex.neuenkirk@gmail.com>
2016-08-16 10:25:58 -0700

    Working on deployment.

--- Minerva/BotConnector/_deploy/playground.py
+++ Minerva/BotConnector/_deploy/playground.py
@@ -4,8 +4,6 @@ import pathlib
 ROOT = pathlib.Path(__file__).absolute().parent.parent.parent
 print(ROOT)
 
-print(os.listdir(str(ROOT)))
-
 for _, _, filenames in os.walk(str(ROOT)):
     for f in filenames:
         print(os.path.join(str(ROOT), f))
